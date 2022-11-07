3069cfd13d0f878ad35aec24aedc201f6fb37db0
myko23 <mykobacal23@gmail.com>
2022-11-07 17:58:15 +0800

    backend

--- src/lib/utils/axios.js
+++ src/lib/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:3001",
+	baseURL: "https://time-report-backend.onrender.com",
 	headers: { "X-Custom-Header": "foobar" },
 });
 
